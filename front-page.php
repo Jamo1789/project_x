0be06531ac898a48b345f9a67090b7ab3e51fd46
jamo1789 <jarmo.laakko@hotmail.com>
2022-08-27 12:49:04 +0300

    second commit

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Album example · Bootstrap v5.0</title>
+    <!--<title>Album example · Bootstrap v5.0</title> -->
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
 
@@ -38,19 +38,33 @@
 
 
 <main>
+  <!--hero with button -->
 
   <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">Album example</h1>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-        <p>
-          <a href="#" class="btn btn-primary my-2">Main call to action</a>
-          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-        </p>
+    <div class="hero-image" style="height: 100%; background-image: url(<?php echo get_theme_file_uri('beach.png') ?>">
+      <div class="hero-text">
+        <h1 style="font-style: Georgia; color: #fff; font-size:50px;">Muutto tulossa?</h1>
+
+        <button>Ota yhteyttä ja kysy tarjous</button>
       </div>
+
+      <div id="popup1" class="overlay">
+        <div class="popup">
+          <a class="close" href="#">&times;</a>
+          <div class="content">
+            <br><br>
+            <h2></h2>
+            
+            <br><br><br>
+          </div>
+        </div>
+      </div>
+      <!--lightbox style popup goes here-->
     </div>
   </section>
+
+  <!--hero with button end -->
+
 
   <div class="album py-5 bg-light">
     <div class="container">
@@ -63,22 +77,24 @@
           the_post();
 
           ?>
-        <div class="col" stye="">
+        <div class="col" style="">
           <div class="card shadow-sm">
-            <?php the_post_thumbnail( 'thumbnail' ); ?>
-            <h3><?php echo get_the_title();?></h3>
-
-
             <div class="card-body">
+            <h3 style="text-align:center;"><?php echo get_the_title();?></h3>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
+              <div class="text-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  <button type="button" class="btn btn-primary align-items-center">Lue lisää</button>
+
                 </div>
-                <small class="text-muted">9 mins</small>
+
               </div>
             </div>
+            <?php the_post_thumbnail(); ?>
+
+
+
+
           </div>
         </div>
         <!--while loop end -->
@@ -97,20 +113,12 @@
   </div>
 
 </main>
-
-<footer class="text-muted py-5">
-  <div class="container">
-    <p class="float-end mb-1">
-      <a href="#">Back to top</a>
-    </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
-  </div>
-</footer>
+<br>
 
 
 
 
 
   </body>
+  <?php get_footer(); ?>
 </html>
