92b5868d55ae62c7e736554b968bce7fbfef2847
jamo1789 <jarmo.laakko@hotmail.com>
2022-09-01 20:57:53 +0300

    almost_there

--- front-page.php
+++ front-page.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<?php get_header(); ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <!--<title>Album example · Bootstrap v5.0</title> -->
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
+<?php get_header(); ?>
+
+    <!--<title>Album example · Bootstrap v5.0</title> -->
 
 
 
@@ -50,7 +41,7 @@
   color: white;">
         <h1 style="font-style: Georgia; color: black; font-size:50px;">Muutto tulossa?</h1>
 
-        <button id="hero_button">Ota yhteyttä ja kysy tarjous</button>
+        <a href="/ota-yhteytta"><button style="font-size: 15px;"  id="hero_button">Kysy tarjous</button></a>
       </div>
 
 
